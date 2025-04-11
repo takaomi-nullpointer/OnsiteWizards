5ec3c5bb327a533326cb5f8427b6a7429f12afa9
takaomi-nullpointer <takaomi.nullpointer@gmail.com>
2025-04-12 04:28:45 +0900

    修正

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import About from "../pages/About";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
